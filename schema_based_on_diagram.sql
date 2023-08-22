24a618bdbff8c01113b014eaff9343cbf464e240
Cosmas Wasswa <cosywas@gmail.com>
2023-08-22 17:07:55 +0300

    Create database clinic, tables based on diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE clinic;
+CREATE TABLE patients(id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR,
+date_of_birth DATE,
+PRIMARY KEY(id)
+);
+
+
+CREATE TABLE medical_histories(id INT GENERATED ALWAYS AS IDENTITY,
+admited_at TIMESTAMP,
+patient_id INT REFERENCES patients(id),
+status VARCHAR,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments(id INT GENERATED ALWAYS AS IDENTITY,
+type VARCHAR,
+name VARCHAR,
+PRIMARY KEY(id)
+);
